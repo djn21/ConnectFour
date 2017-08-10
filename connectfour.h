82a29d91e118f7f7128d1b1c2538418e1aa42125
Dejan Đekanović <djn21@users.noreply.github.com>
2017-08-10 08:57:18 +0200

    ConnectFour_TurnWinnersDiscs name in header fixed

--- connectfour.h
+++ connectfour.h
@@ -43,7 +43,7 @@ unsigned char ConnectFour_CheckWinner();
 * LIMITS:   Parameter onOff must be 1 for ON or 0 for OFF
 * RETURNS:  This function does not return value
 *************************************************************************************************/
-void ConnectForur_TurnWinnersDiscs(unsigned char onOff);
+void ConnectFour_TurnWinnersDiscs(unsigned char onOff);
 
 /*************************************************************************************************
 * DOES:     Start new game
